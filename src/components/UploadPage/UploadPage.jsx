c3cebf9408c260337c79bcc4ad01b410042b3831
Getachew Hundera <ghundera1@gmail.com>
2023-10-16 18:34:10 -0500

    created inputsform and inputs for uploads post, created variables for inputs

--- src/components/UploadPage/UploadPage.jsx
+++ src/components/UploadPage/UploadPage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom"
+import { useState } from "react";
 
 
 
@@ -8,22 +9,58 @@ import Button from '@mui/material/Button';
 
 
 import { ViewMyListPageButton, UploadPageButton } from '../RouteButtons/RouteButtons';
+import { use } from "passport";
 
 
 
 function UploadPage() {
-
+    const [image, setImage] = useState('')
     const [Description, setDescription] = useState('');
-    const [location, setLocation] = useState('');
+    const [houseNumber, setHouseNumber] = useState('');
+    const [streetAddress, setStreetAddress] = useState('');
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
+    const [zipcode, setZipCode] = useState('');
+    const [country, setCountry] = useState('');
     const [price, setPrice] = useState(Number['']);
     const [rating, setRating] = useState(Number['']);
     const [individualSelection, setIndividualSelecton] = useState('');
 
-    const handleSubmit = () => {
 
+    const handleImage = () => {
+        console.log('file event happened');
+        setImage('');
+    }
+
+
+    const handleChangeFor = (value) => {
+        console.log('event happened');
+        setDescription('');
+        setHouseNumber(Number(value));
+        setStreetAddress('');
+        setCity('');
+        setState('');
+        setZipCode(Number(value));
+        setCountry('');
+        setPrice(Number(value));
+        setRating(Number(value));
+        setIndividualSelecton('');
     };
 
-
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('action was dispatched')
+        dispatch({
+            type: 'SEND_POST_SERVER', payload: image, Description, houseNumber, streetAddress,
+            city, state, zipcode, country, price, rating, individualSelection
+        });
+        setImage(null);
+        setDescription(null);
+        setLocation(null);
+        setPrice('');
+        setRating('');
+        setIndividualSelecton('');
+    };
 
 
 
@@ -37,13 +74,89 @@ function UploadPage() {
             <h1> Upload files </h1>
 
             <form>
+
+                <input
+                    type="file"
+                    name="file"
+                    onChange={handleImage}
+                />
                 <input
                     type="text"
+                    style={{ width: '100px', height: '30px' }}
                     placeholder="Description"
-                    Value />
+                    Value={Description}
+                    onChange={(event) => handleChangeFor(event.target.value)}
+                />
+                <input
+                    type="number"
+                    style={{ width: '100px', height: '30px' }}
+                    placeholder="House Number"
+                    value={houseNumber || ''}
+                    onChange={(event) => handleChangeFor(event.target.value)} //will be stored in state as a string
+                />
+                <input
+                    type="text"
+                    style={{ width: '100px', height: '30px' }}
+                    placeholder="Street Address"
+                    value={streetAddress || ''}
+                    onChange={(event) => handleChangeFor(event.target.value)} //will be stored in state as a string
+                />
+                <input
+                    type="text"
+                    style={{ width: '100px', height: '30px' }}
+                    placeholder="City"
+                    value={city || ''}
+                    onChange={(event) => handleChangeFor(event.target.value)} //will be stored in state as a string
+                />
+                <input
+                    type="text"
+                    style={{ width: '100px', height: '30px' }}
+                    placeholder="State"
+                    value={state || ''}
+                    onChange={(event) => handleChangeFor(event.target.value)} //will be stored in state as a string
+                />
+                <input
+                    type="number"
+                    style={{ width: '100px', height: '30px' }}
+                    placeholder="Zipcode"
+                    value={zipcode || ''}
+                    onChange={(event) => handleChangeFor(event.target.value)} //will be stored in state as a string
+                />
+                <input
+                    type="text"
+                    style={{ width: '100px', height: '30px' }}
+                    placeholder="Country"
+                    value={country || ''}
+                    onChange={(event) => handleChangeFor(event.target.value)} //will be stored in state as a string
+                />
+
+                <input
+                    type="number"
+                    style={{ width: '100px', height: '30px' }}
+                    placeholder="Price"
+                    value={price || ''}
+                    onChange={(event) => handleChangeFor(event.target.value)} //will be stored in state as a string
+                />
+
+                <input
+                    type="number"
+                    style={{ width: '100px', height: '30px' }}
+                    min={1} max={10}
+                    placeholder="rating: 1-10"
+                    value={rating || ''}
+                    onChange={(event) => handleChangeFor(event.target.value)} //will be stored in state as a string
+                />
+                <input
+                    type="text"
+                    style={{ width: '100px', height: '30px' }}
+                    placeholder="Solo or Group"
+                    value={individualSelection || ''}
+                    onChange={(event) => handleChangeFor(event.target.value)} //will be stored in state as a string
+                />
 
 
                 <Button
+                    onClick={handleSubmit}
                     component={Link}
                     to={"/UploadSuccessfulPage"}
                     variant="contained"
