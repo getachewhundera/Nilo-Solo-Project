f80be96d9098b75ba01b44cb9a1ff3a85dc05be5
Getachew Hundera <ghundera1@gmail.com>
2023-10-25 19:31:01 -0500

    added a form input and button in the AddListItemPage.jsx file

--- src/components/MapPage/MapPage.jsx
+++ src/components/MapPage/MapPage.jsx
@@ -1,53 +1,53 @@
-import React from "react";
+// import React from "react";
 
-import { AddListItemButton, ViewMyListButton, CompletedButton, MapPageButton } from "../../MyListButtons/MyListButtons";
-import { UploadPageButton } from "../RouteButtons/RouteButtons";
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
-
-
-
-function MapPage() {
-
-    const mapStyles = {
-        height: "100vh",
-        width: "50%"
-    };
-
-    const defaultCenter = {
-        lat: 41.3851, lng: 2.1734
-    }
+// import { AddListItemButton, ViewMyListButton, CompletedButton, MapPageButton } from "../../MyListButtons/MyListButtons";
+// import { UploadPageButton } from "../RouteButtons/RouteButtons";
+// import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 
 
+// function MapPage() {
 
-    return (
-        <>
+//     const mapStyles = {
+//         height: "100vh",
+//         width: "50%"
+//     };
+
+//     const defaultCenter = {
+//         lat: 41.3851, lng: 2.1734
+//     }
+
+
+
+
+//     return (
+//         <>
         
      
         
-            <UploadPageButton />
-            <br></br>
-            <AddListItemButton />
-            <ViewMyListButton />
-            <CompletedButton />
-            <MapPageButton />
-            <h1> this is the Map Page. </h1>
+//             <UploadPageButton />
+//             <br></br>
+//             <AddListItemButton />
+//             <ViewMyListButton />
+//             <CompletedButton />
+//             <MapPageButton />
+//             <h1> this is the Map Page. </h1>
 
-            <LoadScript
-                googleMapsApiKey=''>
-                <GoogleMap
-                    mapContainerStyle={mapStyles}
-                    zoom={13}
-                    center={defaultCenter}
-                />
-            </LoadScript>
-
-
+//             <LoadScript
+//                 googleMapsApiKey=''>
+//                 <GoogleMap
+//                     mapContainerStyle={mapStyles}
+//                     zoom={13}
+//                     center={defaultCenter}
+//                 />
+//             </LoadScript>
 
 
 
-        </>
-    );
-};
 
-export default MapPage; 
+
+//         </>
+//     );
+// };
+
+// export default MapPage; 
