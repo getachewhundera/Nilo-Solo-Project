a15644a5686bbbbdcf1c54c45d49d9e3e337e240
Getachew Hundera <ghundera1@gmail.com>
2023-10-26 19:06:24 -0500

    added useEffect, and map function to

--- src/components/ViewMyListPage/ViewMyListPage.jsx
+++ src/components/ViewMyListPage/ViewMyListPage.jsx
@@ -3,7 +3,8 @@ import React from "react";
 import { AddListItemButton, ViewMyListButton, CompletedButton, MapPageButton } from "../../MyListButtons/MyListButtons.jsx";
 import { UploadPageButton } from "../RouteButtons/RouteButtons.jsx";
 
-
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 
 
@@ -11,23 +12,47 @@ import { UploadPageButton } from "../RouteButtons/RouteButtons.jsx";
 
 function ViewMyListPage() {
 
+    const dispatch = useDispatch();
+    let listItems = useSelector(store => store.rootReducer);
+  
+    // on load, dispatch the saga action
+    useEffect(() => {
+      console.log('in useEffect');
+      const action = { type: 'GET_ADDED_LIST_ITEMS' };
+      dispatch(action);
+    }, []);
+
 
 
 
 
     return (
         <>
-         
-           
             <UploadPageButton />
             <br></br>
-            <AddListItemButton /> 
+            <AddListItemButton />
             <ViewMyListButton />
             <CompletedButton />
             <MapPageButton />
 
 
             <h1> this is the View My List Item Page. </h1>
+
+            <table className="bucket-list-items">
+                <thead>
+                    <tr>
+                        <th>Item</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* Render each item from reducer */}
+                    {.map((, i) => {
+                        return < key={i} ={} />;
+                    })}
+                </tbody>
+            </table>
+
+
 
 
         </>
